cab753cf3add4095a55adbd123eac17329456c89
suassunasaulo <saulosuassuna123@gmail.com>
2023-12-04 11:46:20 -0300

    Aula JS- DADO NUMBER.

---
+++ JS/scripts.js
@@ -0,0 +1,10 @@
+//1-Number
+console.log(2);
+/*Aqui estamos imprimindo o valor 2 no console do navegador.
+ Vale ressaltar que como é um valor que não está dentro de "",
+  ele vai interpretar como number.
+  */
+console.log(typeof 2);
+/*Aqui estamos o tipo de dado no console do navegador
+através do typeof.
+  */
