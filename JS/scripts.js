90bc8faf90fa73123028483860ecd12174eba0dd
suassunasaulo <saulosuassuna123@gmail.com>
2023-12-08 11:46:43 -0300

    Estudos funções e estrutura de condição

--- JS/scripts.js
+++ JS/scripts.js
@@ -8,3 +8,31 @@ console.log(typeof 2);
 /*Aqui estamos o tipo de dado no console do navegador
 através do typeof.
   */
+
+//Operações aritméticas:
+
+console.log(2+5+10/5)
+
+//String
+
+console.log("Um texto")
+console.log('Um texto')
+console.log(`Um texto`)
+
+//Comparações:
+
+console.log(10==10);
+console.log(10===9);
+
+//Funções
+
+const age = prompt ("Digite sua idade:")
+console.log(`Sua idade é ${age} anos`)
+
+//Estrutura de condição IF
+
+const teste = 10
+
+if (teste>=10){
+  console.log("Maior ou igual a 10")
+}
